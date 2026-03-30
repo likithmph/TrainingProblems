c6894dbe78ba0d8a7e8c5ba3af7092e9f7251ac5
likithmph <ar.likith@mphasis.com>
2026-03-30 17:07:35 +0530

    Add files via upload

---
+++ SpringJDBCAnnotations/src/main/java/com/test/main/TestMain.java
@@ -0,0 +1,30 @@
+package com.test.main;
+
+
+import java.util.List;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.test.config.DBConfig;
+import com.test.dao.BookDao;
+import com.test.model.Book;
+
+public class TestMain {
+	public static void main(String[] args) {
+		ApplicationContext ctx = new AnnotationConfigApplicationContext(DBConfig.class);
+				BookDao dao=ctx.getBean(BookDao.class);
+				Book book = new Book();
+				book.setB_id(101);
+				book.setB_name("Brite");
+				book.setB_author("RV");
+				
+				book.setB_price(111);
+				
+//				dao.saveBooks(book);
+//				dao.deleteBooks(book);
+				List<Book> list = dao.listAllBooks();
+				list.forEach(System.out::println);
+				System.out.println("Done");
+	}
+}
