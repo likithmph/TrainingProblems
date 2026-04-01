b83463b343d23713c1f1c92cfa40876c346deb18
likithmph <ar.likith@mphasis.com>
2026-04-01 15:43:13 +0530

    Add files via upload

---
+++ SpringAutoConfig/src/main/java/com/test/service/MyService.java
@@ -0,0 +1,10 @@
+package com.test.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyService {
+	public String message() {
+		return "Custom AutoFig response";
+	}
+}
