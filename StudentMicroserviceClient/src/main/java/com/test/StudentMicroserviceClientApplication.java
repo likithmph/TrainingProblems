b4ca5bd40b4c6c96ab25880257a940c76add8df6
likithmph <ar.likith@mphasis.com>
2026-04-06 10:15:52 +0530

    Add files via upload

---
+++ StudentMicroserviceClient/src/main/java/com/test/StudentMicroserviceClientApplication.java
@@ -0,0 +1,27 @@
+package com.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+import com.test.repository.RemoteStudentRepository;
+
+@SpringBootApplication
+public class StudentMicroserviceClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentMicroserviceClientApplication.class, args);
+	}
+	@Bean
+	@LoadBalanced
+	protected RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+	@Bean
+	public RemoteStudentRepository remoteStudentRepository() {
+		return new RemoteStudentRepository();
+	}
+
+}
